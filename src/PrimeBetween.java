d63fc1d0d6589e6a4b5be15330829c5178a9ee49
Benjapol Worakan <benwrk@live.com>
2016-07-13 10:53:15 +0700

    isPrime calculation bug fix.

--- src/PrimeBetween.java
+++ src/PrimeBetween.java
@@ -56,7 +56,7 @@ public class PrimeBetween {
         if (n <= 1) {
             return false;
         }
-        for (int i = 2; i < Math.sqrt(n); i++) {
+        for (int i = 2; i < Math.sqrt(n + 1); i++) {
             if (n % i == 0) {
                 return false;
             }
